e33149ed7a2215fe463672aab49459e496fc697d
AustinBH <harlowaustin2@gmail.com>
2019-07-30 12:11:44 -0700

    removed unnecessary parentheses

--- src/components/MySongs.js
+++ src/components/MySongs.js
@@ -2,16 +2,14 @@ import React from 'react';
 import SongInfo from './SongInfo'
 
 const MySongs = (props) => {
-    return (
-        <div>
-            <ul>
-                {props.songs.map((song, idx) => {
-                    return <li key={idx}>{song.title}<button onClick={() => props.songInfo(song)}>Display Song Info</button><button onClick={() => props.deleteSong(song)}>Delete Song</button></li>
-                })}
-            </ul>
-            <SongInfo songInfo={props.displaySongInfo} loading={props.loading} />
-        </div>
-    )
+    return <div>
+                <ul>
+                    {props.songs.map((song, idx) => {
+                        return <li key={idx}>{song.title}<button onClick={() => props.songInfo(song)}>Display Song Info</button><button onClick={() => props.deleteSong(song)}>Delete Song</button></li>
+                    })}
+                </ul>
+                <SongInfo songInfo={props.displaySongInfo} loading={props.loading} />
+            </div>
 }
 
 export default MySongs
