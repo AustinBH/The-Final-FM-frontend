24c85f9c5194599fb1fbe8398bf1c6326ff80094
AustinBH <harlowaustin2@gmail.com>
2019-09-11 12:14:55 -0700

    updated song info to display title, artist, and album information

--- src/components/SongInfo.js
+++ src/components/SongInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ListGroup } from 'react-bootstrap'
+import { ListGroup, Table } from 'react-bootstrap'
 import logo from '../logo.svg';
 
 const SongInfo = (props) => {
@@ -8,7 +8,33 @@ const SongInfo = (props) => {
             <img src={logo} className='App-logo' alt='loading-logo' />
         </p> :
             <ListGroup>
-                {props.songInfo[0] ? <ListGroup.Item >{props.songInfo[0].artist.name + "'s upcoming concerts"}</ListGroup.Item> : null}                {props.songInfo[1] && props.songInfo[1].map((event, idx) => {
+                {props.songInfo[0] ? 
+                <ListGroup.Item >
+                    <Table bordered size='sm'>
+                        <thead>
+                                <tr>
+                                    <th>Title:</th>
+                                    <th>Album:</th>
+                                    <th>Artist:</th>
+                                </tr>
+                        </thead>   
+                        <tbody>
+                                <tr>
+                                    <td>
+                                        {props.songInfo[0].title}
+                                    </td>
+                                    <td>
+                                        {props.songInfo[0].album_name}
+                                    </td>
+                                    <td>
+                                        {props.songInfo[0].artist.name}
+                                    </td>
+                                </tr>
+                        </tbody>
+                    </Table>
+                    {props.songInfo[0].artist.name}'s upcoming concerts
+                </ListGroup.Item> : null}
+                {props.songInfo[1] && props.songInfo[1].map((event, idx) => {
                     if (event.url) {
                         return <ListGroup.Item className="list-group-item"  key={idx} >
                             <a href={event.url}  rel="noopener noreferrer"  target="_blank">
